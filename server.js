df9b5882ae0de2f6bdc672fc72e251f61f46aaf8
funatutsav <funatutsav@gmail.com>
2017-02-16 17:33:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send("article two is requested and will be served here!"));
+  res.send("article two is requested and will be served here!");
 });
 
 app.get('/article-three', function (req, res) {
